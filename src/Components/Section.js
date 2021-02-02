e20f286f2d8c382fdd49b838da61faadd07234a7
parksumz <parksumz@d2x.co.kr>
2021-02-02 11:49:35 +0900

    Section

---
+++ src/Components/Section.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div`
+  :not(:last-child) {
+    margin-bottom: 40px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 15px;
+`;
+
+const Grid = styled.div`
+  margin-top: 10px;
+`;
+
+const Section = ({ title, children }) => (
+  <Container>
+    <Title>{title}</Title>
+    <Grid>{children}</Grid>
+  </Container>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default Section;
